1602edffb88c8f3d429e6b081f70fa773b4a7458
Rizki Alfariz Ramadhan <ralfariz780@gmail.com>
2024-05-21 14:10:58 +0700

    feat : add map6

--- editor.py
+++ editor.py
@@ -30,7 +30,7 @@ class Editor:
         self.tilemap = Tilemap(self, tile_size=16)
         
         try:
-            self.tilemap.load('map5.json')
+            self.tilemap.load('map6.json')
         except FileNotFoundError:
             pass
         
@@ -125,7 +125,7 @@ class Editor:
                     if event.key == pygame.K_t:
                         self.tilemap.autotile()
                     if event.key == pygame.K_o:
-                        self.tilemap.save('map5.json')
+                        self.tilemap.save('map6.json')
                     if event.key == pygame.K_LSHIFT:
                         self.shift = True
                 if event.type == pygame.KEYUP:
